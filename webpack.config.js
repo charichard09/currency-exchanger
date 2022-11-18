6d544c94e7ad1d60ae1d2f1da42f66e2c89f15e2
Richard <charichard09@gmail.com>
2022-11-18 08:29:40 -0800

    install and update dev environment for project

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'name',
+      title: 'Currency Exchanger',
       template: './src/index.html',
       inject: 'body'
     }),
