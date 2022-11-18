4dc593813fa7edcf170feb23244bb42b1ab4f143
Richard <charichard09@gmail.com>
2022-11-18 13:51:25 -0800

    add code to pass test 1 2 3 4 of Jest

--- __tests__/is-not-data-type.test.js
+++ __tests__/is-not-data-type.test.js
@@ -6,11 +6,15 @@ describe("check if argument is not data type", () => {
     expect(isNotDataType("Hello", "numberString")).toEqual(true);
   });
 
-  test("2. It should return true if value 1 is not string", () => {
-    expect(isNotDataType(1, "string")).toEqual(true);
+  test("2. It should return true if value blank is not string", () => {
+    expect(isNotDataType("", "string")).toEqual(true);
   });
 
   test("3. It should return false if value 'AUD' is a string", () => {
+    expect(isNotDataType("AUD", "string")).toEqual(false);
+  });
+
+  test(". It should return false if value '1' is a numberString", () => {
     expect(isNotDataType("1", "numberString")).toEqual(false);
   });
 });
