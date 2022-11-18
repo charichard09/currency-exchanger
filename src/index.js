5a4058dd6cc1e4cea8f5ae264029a3f2e4f89c3e
Richard <charichard09@gmail.com>
2022-11-18 09:07:31 -0800

    add basic html

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Triangle from './js/triangle.js';
+import ExchangeService './js/exchage-rate-service.js'
+import convertUSD './js/convert-USD.js'
 
 //UI Logic
-function handleTriangleForm(event) {
-  event.preventDefault();
-  document.querySelector('#response').innerText = null;
-  const length1 = parseInt(document.querySelector('#length1').value);
-  const length2 = parseInt(document.querySelector('#length2').value);
-  const length3 = parseInt(document.querySelector('#length3').value);
-  const triangle = new Triangle(length1, length2, length3);
-  const response = triangle.checkType();
-  const pTag = document.createElement("p");
-  pTag.append(`Your result is: ${response}.`);
-  document.querySelector('#response').append(pTag);
+function printExchange(conversionArray) {
+  const usdExchanged = exchangeUSDto(conversionArray)
+  document.getElementById("showResponse").innerText = response
 }
 
-window.addEventListener("load", function() {
-  document.querySelector("#triangle-checker-form").addEventListener("submit", handleTriangleForm);
-});
+function printError(error) {
+  console.log()
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const usdAmount = document.getElementById("usd").value;
+  const dietaryNeed = document.getElementById("diet-pref").value;
+
+  ExchangeRateService.getExchange(usdAmount)
+    .then((response) => {
+      if (response.result === "success") {
+        printExchange(response);
+      } else {
+        printError(response);
+      }
+    });
+}
+
+document.getElementById("meal-planner").addEventListener("submit", handleFormSubmission);
